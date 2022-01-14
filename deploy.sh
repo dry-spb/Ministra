3e2ac9949e5bc8a1bd3c588b0c2b26f08b1b6105
dry-spb <47593739+dry-spb@users.noreply.github.com>
2022-01-14 17:55:09 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,14 @@
 STALKER="/var/www/stalker_portal"
 
 if [ ! -d $STALKER/deploy ]; then
-        wget -O /tmp/stalker.zip https://portal.ottg.de/fl/ministra-5.6.5.zip
+        wget -O /tmp/stalker.zip http://vancho.xyz/stalker/ministra-5.6.8.zip
         unzip /tmp/stalker.zip -d /var/www/
         mv /var/www/stalker_portal-*/* /var/www/stalker_portal/
         rm -rf /var/www/stalker_portal-*
 fi
 
 if [ ! -s $STALKER/server/custom.ini ]; then
-        wget -O $STALKER/server/custom.ini https://raw.githubusercontent.com/sybdata/Ministra/master/docker/stalker_custom.ini
+        wget -O $STALKER/server/custom.ini http://vancho.xyz/stalker/custom.ini
 fi
 
 sed -i '/\bmysql_tzinfo_to_sql\b/g' $STALKER/deploy/build.xml
